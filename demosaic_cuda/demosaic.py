145c5a19c660e9281549eb6940be4c5dad528e27
Hayato <hayaton@bu.edu>
2020-12-14 12:06:53 -0500

    fixed indentation

--- demosaic_cuda/demosaic.py
+++ demosaic_cuda/demosaic.py
@@ -394,8 +394,8 @@ class Demosaic:
 		self.bdim_x = int(math.ceil(im.shape[0]/2))
 		self.bdim_y = int(math.ceil(im.shape[1]/2))
 
-    	bdim = max(bdim_x, bdim_y)
-    	self.block_dim = (4,4,1)
+		bdim = max(bdim_x, bdim_y)
+		self.block_dim = (4,4,1)
 		self.grid_dim = (bdim,bdim,1)
 
 	def demosaic_nn_GPU(self):
